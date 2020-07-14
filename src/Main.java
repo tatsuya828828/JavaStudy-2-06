633d72329786ab25a873a9aedca887192bfce5b9
tatsuya828828 <kkjy011894@gmail.com>
2020-07-14 18:16:29 +0900

    [finish]06-03

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,21 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
 	 public static void main(String[] args) {
-		 // この行が実行された瞬間関数の実態が生み出される
-		 // この構文は、ラムダ式構文という
-		 // 引数の方を省略することもできる
-		 MyFunction func = (a, b) -> {
-			 return a-b;
-		};
-		 // 変数funcに格納されている処理ロジックを、引数5と3で実行する
-		 // インタフェースのメソッドで呼び出し
-		 int a = func.call(5, 3);
-		 System.out.println("5-3は"+a);
+		 List<Integer> list = new ArrayList<>();
+		 list.add(2);
+		 list.add(4);
+		 for(Integer i: list) {
+			 System.out.println(i*2);
+		 }
+		 // 上記のコードを、ラムダ式に書き換えた場合
+		 // ArrayListやHashSetなどはJava8からstreamメソッドを持つようになった
+		 // このメソッドを呼び出すと、各要素に様々な一括処理を行ったり、集計を行うためのメソッドを豊富に持ったStreamインスタンスを得ることができる
+		 // Streamインスタンスが持つ代表的なメソッドがforEachである
+		 // このメソッドは、引数1つの巻数オブジェクトを受け取り、コレクションの各要素に対して関数を繰り返し実行してくれます
+		 list.stream().forEach(i -> System.out.println(i*2));
+		 // parallelStreamメソッドを使うとJVMは並列処理を行うようになるため大きく性能が向上することがある
+		 list.parallelStream().forEach(i -> System.out.println(i*2));
 	 }
 }
